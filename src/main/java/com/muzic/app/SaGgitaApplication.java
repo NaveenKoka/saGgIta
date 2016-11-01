18ce96e1ed4db674a97d032250dfa7d9140c1478
NaveenKoka <koka.naveen@gmail.com>
2016-11-01 00:04:29 -0700

    Initial Commit

---
+++ src/main/java/com/muzic/app/SaGgitaApplication.java
@@ -0,0 +1,12 @@
+package com.muzic.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaGgitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaGgitaApplication.class, args);
+	}
+}
